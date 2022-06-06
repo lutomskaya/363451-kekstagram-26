daf8d3028d973a17bbb6a5707c3ea55a3ffdbe8b
lutomskaya <lutomskaya@gmail.com>
2022-06-06 15:08:23 +0300

    Еще правки линтера

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 function getRandom (min, max)  {
   min = Math.ceil(min);
   max = Math.floor(max);
-  
   if (min < max) {
     const random = Math.round((Math.random() * (max - min)) + min);
-    return random;  
+    return random;
   }
-
-  return "Первое число не может быть больше второго"
+  return 'Первое число не может быть больше второго';
 }
 
-console.log(getRandom (25, 20))
+getRandom (25, 20);
 
 function checkStringLength (text, maxLength) {
-  return (text.length <= maxLength);    
+  return (text.length <= maxLength);
 }
 
-console.log(checkStringLength('Какой-то комментарий', 140))
+checkStringLength('Какой-то комментарий', 140);
